1475b230ca53e978fb8f20a8ec2ab7fac5d15ee4
Aditya S T P <s528763@nwmissouri.edu>
2018-03-08 08:52:24 +0000

    Aditya S T P: Changed all-compile.ps1 for excluding folders starting with . (dot) like in .vscode

--- all-compile.ps1
+++ all-compile.ps1
@@ -2,6 +2,6 @@
 # PowerShell Script to generate all our jar files 
 # Uses Get-ChildItem cmdlet, and filters using the -Directory switch
 
-Get-ChildItem  -Directory  | ForEach-Object { Write-Host "$_";
+Get-ChildItem -Exclude .* -Directory  | ForEach-Object { Write-Host "$_";
   cd $_; mvn clean compile assembly:single; cd ..; 
 }
